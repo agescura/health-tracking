2dcd0124557bc2d00f98be498764884d41d7d987
Albert Gil Escura <>
2024-07-29 09:46:01 -0500

    create xcode project

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Albert Gil Escura on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
